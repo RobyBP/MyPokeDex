2191796f1dd03146d3cd0d73eb5cea82846d0e6b
Robert Baricevic <robert.baricevicpetrus@gmail.com>
2021-08-09 19:58:06 +0200

    Added Bottom navigation

--- app/src/main/java/com/example/mypokedex/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.mypokedex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNavigation, navHostFragment.navController)
+        bottomNavigation.itemIconTintList = null
     }
 }
