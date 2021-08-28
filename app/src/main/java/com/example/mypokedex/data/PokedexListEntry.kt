d913c416ed3e1ca7d1055d197d3c05f8f1c9d186
Robert Baricevic <robert.baricevicpetrus@gmail.com>
2021-08-28 20:31:10 +0200

    Added a dominant color parameter so it doesn't get calculated in the adapter

--- app/src/main/java/com/example/mypokedex/data/PokedexListEntry.kt
+++ app/src/main/java/com/example/mypokedex/data/PokedexListEntry.kt
@@ -3,5 +3,6 @@ package com.example.mypokedex.data
 data class PokedexListEntry(
     val pokemonName: String,
     val imageUrl: String,
-    val number: Int
+    val number: Int,
+    val dominantColor: Int
 )
