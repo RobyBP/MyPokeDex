4d0fe7fbbed7cc081db3946063e626e61c43f94b
Robert Baricevic <robert.baricevicpetrus@gmail.com>
2021-08-29 21:12:22 +0200

    Added the loadPokemonDetail function

--- app/src/main/java/com/example/mypokedex/database/PokemonRepository.kt
+++ app/src/main/java/com/example/mypokedex/database/PokemonRepository.kt
@@ -1,6 +1,7 @@
 package com.example.mypokedex.database
 
 import com.example.mypokedex.data.remote.PokeApi
+import com.example.mypokedex.data.remote.response.Pokemon
 import com.example.mypokedex.data.remote.response.PokemonList
 import com.example.mypokedex.util.NetworkViewState
 import dagger.hilt.android.scopes.ActivityScoped
@@ -9,10 +10,19 @@ import javax.inject.Inject
 @ActivityScoped
 class PokemonRepository @Inject constructor(
     private val pokeApi: PokeApi
-){
+) {
     suspend fun getPokemonList(limit: Int, offset: Int): NetworkViewState<PokemonList> {
         val response = try {
             pokeApi.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return NetworkViewState.Error(null, e.message)
+        }
+        return NetworkViewState.Success(response)
+    }
+
+    suspend fun getPokemonDetail(name: String): NetworkViewState<Pokemon> {
+        val response = try {
+            pokeApi.getPokemonInfo(name)
         } catch (e: Exception) {
             return NetworkViewState.Error(null, e.message)
         }
