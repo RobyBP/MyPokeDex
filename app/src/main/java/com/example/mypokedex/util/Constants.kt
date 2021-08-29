e93e32ca5fb6b5820f7c0ab5c3fcd325b930c8b8
Robert Baricevic <robert.baricevicpetrus@gmail.com>
2021-08-29 20:58:57 +0200

    Added more constants and removed PokemonNumberKey

--- app/src/main/java/com/example/mypokedex/util/Constants.kt
+++ app/src/main/java/com/example/mypokedex/util/Constants.kt
@@ -3,4 +3,7 @@ package com.example.mypokedex.util
 object Constants {
     const val baseUrl = "https://pokeapi.co/api/v2/"
     const val PAGE_SIZE = 10
+    const val POKEMON_NAME_KEY = "name"
+    const val POKEMON_URL_KEY = "url"
+    const val POKEMON_DOMINANT_COLOR_KEY = "dominant color"
 }
